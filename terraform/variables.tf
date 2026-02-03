4a42d998d60d5df26669ab25fd361b2c08905d49
Saurabh-Tripathii <98814319+Saurabh-Tripathii@users.noreply.github.com>
2026-02-04 03:08:44 +0530

    variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,9 @@
-variable "region" {
-  default = "us-east-1"
-}
-
 variable "raw_bucket_name" {
-  default = "liquor-sales-analytics-raw-data-db"
+  description = "Raw data S3 bucket"
+  type        = string
 }
 
 variable "clean_bucket_name" {
-  default = "liquor-sales-cleaned-dataa"
-}
-
-variable "glue_job_name" {
-  default = "liquor-sales-cleaning-job-copy"
+  description = "Clean data S3 bucket"
+  type        = string
 }
