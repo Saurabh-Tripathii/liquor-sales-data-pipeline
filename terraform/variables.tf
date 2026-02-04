a82a4c0a5e2da2cfc01c91263079c6bb2a221f7d
Saurabh-Tripathii <98814319+Saurabh-Tripathii@users.noreply.github.com>
2026-02-05 01:41:51 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,20 +1,14 @@
 variable "raw_bucket_name" {
-  description = "Raw S3 bucket name"
+  description = "S3 bucket for raw liquor sales data"
   type        = string
 }
 
 variable "clean_bucket_name" {
-  description = "Cleaned S3 bucket name"
+  description = "S3 bucket for cleaned liquor sales data"
   type        = string
 }
 
 variable "glue_script_bucket" {
-  description = "S3 bucket for Glue scripts"
+  description = "S3 bucket where glue scripts are stored"
   type        = string
-}
-
-variable "glue_job_name" {
-  description = "Glue Job Name"
-  type        = string
-  default     = "liquor-sales-cleaning-job"
 }
