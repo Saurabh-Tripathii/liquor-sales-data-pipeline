093e9d3f9e66dac4e90ecf6f2a1a72c2f264f9f6
Saurabh-Tripathii <98814319+Saurabh-Tripathii@users.noreply.github.com>
2026-02-05 00:34:26 +0530

    main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,21 +1,5 @@
 provider "aws" {
-  region = "ap-south-1"
-}
-
-# -------------------------
-# S3 RAW BUCKET
-# -------------------------
-resource "aws_s3_bucket" "raw_bucket" {
-  bucket        = var.raw_bucket_name
-  force_destroy = true
-}
-
-# -------------------------
-# S3 CLEAN BUCKET
-# -------------------------
-resource "aws_s3_bucket" "clean_bucket" {
-  bucket        = var.clean_bucket_name
-  force_destroy = true
+  region = var.aws_region
 }
 
 # -------------------------
@@ -28,18 +12,62 @@ resource "aws_iam_role" "glue_role" {
     Version = "2012-10-17"
     Statement = [{
       Effect = "Allow"
-      Principal = {
-        Service = "glue.amazonaws.com"
-      }
+      Principal = { Service = "glue.amazonaws.com" }
       Action = "sts:AssumeRole"
     }]
   })
 }
 
-# -------------------------
-# ATTACH GLUE POLICY
-# -------------------------
 resource "aws_iam_role_policy_attachment" "glue_policy" {
   role       = aws_iam_role.glue_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
+}
+
+# -------------------------
+# GLUE JOB
+# -------------------------
+resource "aws_glue_job" "liquor_job" {
+  name     = "liquor-sales-cleaning-job"
+  role_arn = aws_iam_role.glue_role.arn
+
+  glue_version = "4.0"
+
+  command {
+    name            = "glueetl"
+    python_version  = "3"
+    script_location = var.glue_script_s3_path
+  }
+
+  default_arguments = {
+    "--job-language" = "python"
+  }
+}
+
+# -------------------------
+# GLUE CRAWLER
+# -------------------------
+resource "aws_glue_crawler" "clean_crawler" {
+  name          = "liquor-cleaned-crawler"
+  role          = aws_iam_role.glue_role.arn
+  database_name = aws_glue_catalog_database.athena_db.name
+
+  s3_target {
+    path = var.clean_s3_base_path
+  }
+}
+
+# -------------------------
+# ATHENA DATABASE
+# -------------------------
+resource "aws_glue_catalog_database" "athena_db" {
+  name = var.athena_db_name
+}
+
+# -------------------------
+# ATHENA VIEWS
+# -------------------------
+resource "aws_athena_named_query" "yearly_trend_view" {
+  name      = "vw_yearly_trend"
+  database  = aws_glue_catalog_database.athena_db.name
+  query     = file("${path.module}/views/vw_yearly_trend.sql")
 }
