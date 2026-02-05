bf77fcb8d74baaaecfd6875db6075245bc23c15b
Saurabh-Tripathii <98814319+Saurabh-Tripathii@users.noreply.github.com>
2026-02-05 11:10:57 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,12 +1,13 @@
 output "raw_bucket" {
-  value = aws_s3_bucket.raw_bucket.bucket
+  value = coalesce(
+    try(aws_s3_bucket.raw_bucket[0].bucket, null),
+    data.aws_s3_bucket.raw_existing.bucket
+  )
 }
 
 output "clean_bucket" {
-  value = aws_s3_bucket.clean_bucket.bucket
+  value = coalesce(
+    try(aws_s3_bucket.clean_bucket[0].bucket, null),
+    data.aws_s3_bucket.clean_existing.bucket
+  )
 }
-
-output "glue_job_name" {
-  value = aws_glue_job.liquor_job.name
-}
-
