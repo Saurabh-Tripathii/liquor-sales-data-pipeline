3576cbda319874fa4618d1837d7a014b243c696c
Saurabh-Tripathii <98814319+Saurabh-Tripathii@users.noreply.github.com>
2026-02-05 14:45:38 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,28 +11,33 @@ data "aws_iam_role" "glue_role" {
 
 # =========================
 # GLUE JOB (ONLY DEFINE)
-# =========================
 resource "aws_glue_job" "liquor_job" {
-  name     = "liquor-sales-cleaning-job"
+  name     = "liquor-sales-cleaning-job"   # SAME NAME
   role_arn = data.aws_iam_role.glue_role.arn
-
-  glue_version = "4.0"
-  worker_type  = "G.2X"
-  number_of_workers = 5
 
   command {
     name            = "glueetl"
-    script_location = "s3://my-glue-scripts-s/scripts/liquor_cleaning_job.py"
+    script_location = "s3://liquor-glue-scripts-auto/scripts/liquor_cleaning_job.py"
     python_version  = "3"
   }
 
   default_arguments = {
+    "--RAW_S3_PATH"   = var.raw_s3_path
+    "--CLEAN_S3_PATH" = var.clean_s3_path
     "--job-language" = "python"
   }
 
-  max_retries = 0
-  timeout     = 10
+  glue_version      = "4.0"
+  worker_type       = "G.2X"
+  number_of_workers = 5
+
+  lifecycle {
+    ignore_changes = [
+      default_arguments
+    ]
+  }
 }
+
 
 # =========================
 # GLUE CRAWLER (EXISTING DB)
