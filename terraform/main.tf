3d8a4acbbc12f052d7de019c633be0731ba54ce6
Saurabh-Tripathii <98814319+Saurabh-Tripathii@users.noreply.github.com>
2026-02-05 08:57:18 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,21 +35,11 @@ resource "aws_s3_object" "glue_script_upload" {
 }
 
 # IAM ROLE FOR GLUE
-
-resource "aws_iam_role" "glue_role" {
-  name = "AWSGlueServiceRole-liquor"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = { Service = "glue.amazonaws.com" }
-        Action    = "sts:AssumeRole"
-      }
-    ]
-  })
+resource "aws_iam_role_policy_attachment" "glue_policy_attach" {
+  role       = aws_iam_role.glue_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSGlueServiceRole"
 }
+
 
 
 # ATTACH AWS MANAGED GLUE POLICY
