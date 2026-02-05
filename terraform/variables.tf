b60cc738c6c6f6a9d5ed4fd7684be4c9cb8ebee3
Saurabh-Tripathii <98814319+Saurabh-Tripathii@users.noreply.github.com>
2026-02-05 11:29:43 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,14 @@
-variable "raw_bucket_name" {
-  type = string
+variable "aws_region" {
+  default = "us-east-1"
 }
 
-variable "clean_bucket_name" {
-  type = string
+variable "raw_bucket_name" {}
+variable "clean_bucket_name" {}
+
+variable "glue_job_name" {
+  default = "liquor-sales-cleaning-job"
 }
 
 variable "glue_script_bucket" {
-  type = string
-}
-
-variable "clean_s3_path" {
-  type = string
+  default = "liquor-glue-scripts-auto"
 }
