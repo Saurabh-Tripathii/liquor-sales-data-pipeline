9a7907c4af68ac51628d38b1a21b8cbd8a0b09a1
Saurabh-Tripathii <98814319+Saurabh-Tripathii@users.noreply.github.com>
2026-02-07 01:01:49 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,9 @@
 variable "raw_s3_path" {
-  description = "RAW data S3 path"
   type        = string
+  description = "RAW data S3 path"
 }
 
-#variable "clean_s3_path" {
-  description = "CLEAN data S3 path"
+variable "clean_s3_path" {
   type        = string
+  description = "CLEAN data S3 path"
 }
